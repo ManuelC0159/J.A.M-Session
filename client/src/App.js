c98099a7087e07688766483872c7c2141d46e0a3
Jesse Osborne <jesseosborne615@gmail.com>
2023-04-25 21:39:47 -0400

    added profile base

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import useFetch from './hook/useFetch'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-// import Signup from './pages/Signup';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
     <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/login" element={<Login />} />
-    {/* <Route path="/signup" element={<Signup />} /> */}
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/profile" element={<Profile />} />
     </Routes>
     </BrowserRouter>
     
